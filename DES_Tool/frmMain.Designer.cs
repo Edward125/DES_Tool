0f373ebbff1b736b3f18e5c8f1bb51f6f933acc8
Edward <edward_song@yeah.net>
2017-03-30 14:32:33 +0800

    1st commit,v1.0

---
+++ DES_Tool/frmMain.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace DES_Tool
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txtOri = new System.Windows.Forms.TextBox();
+            this.txtKey1 = new System.Windows.Forms.TextBox();
+            this.txtEncrypt1 = new System.Windows.Forms.TextBox();
+            this.txtDecrypt1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtOri
+            // 
+            this.txtOri.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOri.Location = new System.Drawing.Point(116, 20);
+            this.txtOri.Name = "txtOri";
+            this.txtOri.Size = new System.Drawing.Size(255, 26);
+            this.txtOri.TabIndex = 0;
+            // 
+            // txtKey1
+            // 
+            this.txtKey1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKey1.Location = new System.Drawing.Point(116, 69);
+            this.txtKey1.MaxLength = 8;
+            this.txtKey1.Name = "txtKey1";
+            this.txtKey1.Size = new System.Drawing.Size(255, 26);
+            this.txtKey1.TabIndex = 1;
+            // 
+            // txtEncrypt1
+            // 
+            this.txtEncrypt1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEncrypt1.Location = new System.Drawing.Point(116, 117);
+            this.txtEncrypt1.Name = "txtEncrypt1";
+            this.txtEncrypt1.Size = new System.Drawing.Size(255, 26);
+            this.txtEncrypt1.TabIndex = 2;
+            // 
+            // txtDecrypt1
+            // 
+            this.txtDecrypt1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDecrypt1.Location = new System.Drawing.Point(116, 172);
+            this.txtDecrypt1.Name = "txtDecrypt1";
+            this.txtDecrypt1.ReadOnly = true;
+            this.txtDecrypt1.Size = new System.Drawing.Size(255, 26);
+            this.txtDecrypt1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDecrypt);
+            this.groupBox1.Controls.Add(this.btnEncrypt);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtOri);
+            this.groupBox1.Controls.Add(this.txtDecrypt1);
+            this.groupBox1.Controls.Add(this.txtKey1);
+            this.groupBox1.Controls.Add(this.txtEncrypt1);
+            this.groupBox1.Location = new System.Drawing.Point(28, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(404, 287);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecrypt.Location = new System.Drawing.Point(237, 230);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(103, 32);
+            this.btnDecrypt.TabIndex = 16;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncrypt.Location = new System.Drawing.Point(37, 230);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(103, 32);
+            this.btnEncrypt.TabIndex = 15;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(17, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 21);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "一次还原";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(17, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "一次加密";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(33, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Key";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(33, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "原文";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 325);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "DES Tool";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOri;
+        private System.Windows.Forms.TextBox txtKey1;
+        private System.Windows.Forms.TextBox txtEncrypt1;
+        private System.Windows.Forms.TextBox txtDecrypt1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DES_Tool/frmMain.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DES_Tool
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            string temp = string.Empty;
+            if (string.IsNullOrEmpty(txtOri.Text))
+                return;
+            if (string.IsNullOrEmpty(txtKey1.Text ))
+                return;
+            if (this.txtKey1.Text.Length != 8)
+                return;
+            //一次加密
+            this.txtEncrypt1.Text = DES.DesEncrypt(this.txtOri.Text.Trim(), this.txtKey1.Text.Trim());
+            //temp = this.txtEncrypt1.Text;
+            //if (string.IsNullOrEmpty(txtKey2.Text))
+            //    return;
+            //if (this.txtKey2.Text.Length != 8)
+            //    return;
+
+            //this.txtEncrypt2.Text = DES.DesEncrypt(temp, this.txtKey2.Text.Trim());
+            
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            string temp = string.Empty;
+            if (string.IsNullOrEmpty(txtKey1.Text ))
+                return;
+            if (this.txtKey1.Text.Length != 8)
+                return;
+            if (!string.IsNullOrEmpty(this.txtEncrypt1.Text))
+            {
+                this.txtDecrypt1.Text = DES.DesDecrypt(this.txtEncrypt1.Text, this.txtKey1.Text);
+                temp = this.txtDecrypt1.Text;
+
+            }
+
+            
+
+
+            //if (string.IsNullOrEmpty(this.txtKey2.Text))
+            //    return;
+            //if (this.txtKey2.Text.Length != 8)
+            //    return;
+            //if (string.IsNullOrEmpty(this.txtEncrypt2.Text))
+            //    return;
+            //this.txtDecrypt2.Text = DES.DesDecrypt(temp, this.txtKey2.Text);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.Text = "DES Tool,Ver:" + Application.ProductVersion + ",Author:Edward Song";
+        }
+    }
+}
